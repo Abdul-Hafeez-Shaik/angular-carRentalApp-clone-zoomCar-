cdcde795feaf8e9397b525461e8ffeed8e4ab90f
Abdul-Hafeez-Shaik <abdulhafeez43949@gmail.com>
2025-05-12 12:49:20 +0530

    navbar and component creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { BookingComponent } from './pages/booking/booking.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MyBookingComponent } from './pages/my-booking/my-booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'booking',
+        component: BookingComponent
+    },
+    {
+        path: 'search',
+        component: SearchComponent
+    },
+    {
+        path: 'my-bookings',
+        component: MyBookingComponent
+    }
+];
